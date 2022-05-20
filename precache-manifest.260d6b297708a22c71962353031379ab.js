cb92b7c2af1b84816fa27faacf323ae9eecdad5f
stplva <sofiia.teplova@gmail.com>
2022-05-20 15:06:56 +0200

    Updates

---
+++ precache-manifest.260d6b297708a22c71962353031379ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "541e27cb076be022e55d",
+    "url": "/starnavi-test-task/static/css/main.6d8362b4.chunk.css"
+  },
+  {
+    "revision": "541e27cb076be022e55d",
+    "url": "/starnavi-test-task/static/js/main.541e27cb.chunk.js"
+  },
+  {
+    "revision": "ff2aeb385757ea353d47",
+    "url": "/starnavi-test-task/static/js/1.ff2aeb38.chunk.js"
+  },
+  {
+    "revision": "9c887821d62a22411515",
+    "url": "/starnavi-test-task/static/js/runtime~main.9c887821.js"
+  },
+  {
+    "revision": "f0296d631db9dd3c5431385d1177b930",
+    "url": "/starnavi-test-task/index.html"
+  }
+];
